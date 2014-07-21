cf419f73fd76d6528860abb36c1a1d6209ef1eb4
Christopher Campbell <campbell.chris.222@gmail.com>
2014-07-22 03:45:15 +1000

    Added some pointless code as I mess around with git

---
+++ tempproject/__init__.py
@@ -0,0 +1,5 @@
+print "Hello This is a test."
+usersname = raw_input("Hello, please enter your name: ")
+print "Hello %s. How are you today? " % (usersname)
+raw_input()
+print "Ok, I don't really care. I'm a computer."
